6678c4bd6d747b082ce1fa30a15d76a4213ad118
Charles Moss & Brian Borealis <student@epicodus.com>
2015-09-09 11:36:30 -0700

    all pigLatin tests pass, fail 1st sentanceMill test

--- js/main.js
+++ js/main.js
@@ -2,17 +2,29 @@ var pigLatin = function(input) {
 
 
     var firstLetter = input.slice(0, 1);
-    var vowels = ["a", "e", "i","o","u"];
+    var firstTwoLetters = input.slice(0, 2);
+    var firstThreeLetters = input.slice(0, 3);
+    var vowels = ["a", "e", "i", "o", "u"];
+    var digraphs = ["bl", "br", "ch", "ck", "cl", "cr", "dr", "fl", "fr", "gh", "gl", "gr", "ng", "ph", "pl", "pr", "qu", "sc", "sh", "sk", "sl", "sm", "sn", "sp", "st", "sw", "th", "tr", "tw", "wh", "wr"];
+    var trigraphs = ["nth", "sch", "scr", "shr", "spl", "spr", "squ", "str", "thr"]
 
     if (vowels.indexOf(firstLetter) > -1) {
 
         input = input + "ay";
+
+    } else if (trigraphs.indexOf(firstThreeLetters) > -1) {
+
+        input = input.slice(3) + firstThreeLetters + "ay";
+
+    } else if (digraphs.indexOf(firstTwoLetters) > -1) {
+
+        input = input.slice(2) + firstTwoLetters + "ay";
+
     } else {
 
         input = input.slice(1) + firstLetter + "ay";
-    }
-
-
+    };
 
     return input;
+
 };
--- spec/specs.js
+++ spec/specs.js
@@ -1,19 +1,28 @@
 describe('pigLatin', function() {
-  it("enter a string and recieve a string", function() {
-    expect(pigLatin("pig")).to.be.a("string");
-  });
+    it("enter a string and recieve a string", function() {
+        expect(pigLatin("pig")).to.be.a("string");
+    });
 
-  it("adds -ay to words that start with a vowel", function() {
-    expect(pigLatin("apple")).to.equal("appleay");
-  });
+    it("adds -ay to words that start with a vowel", function() {
+        expect(pigLatin("apple")).to.equal("appleay");
+    });
 
-  it("move constant at the begining of a word to the end and adds -ay", function() {
-    expect(pigLatin("dog")).to.equal("ogday");
-  });
+    it("move constant at the begining of a word to the end and adds -ay", function() {
+        expect(pigLatin("dog")).to.equal("ogday");
+    });
 
-  it("move constants at the begining of a word to the end and adds -ay", function() {
-    expect(pigLatin("chai")).to.equal("aichay");
-  });
+    it("move two constants at the begining of a word to the end and adds -ay", function() {
+        expect(pigLatin("chai")).to.equal("aichay");
+    });
 
+    it("move three constants at the begining of a word to the end and adds -ay", function() {
+        expect(pigLatin("scream")).to.equal("eamscray");
+    });
 
+});
+
+describe('sentanceMill', function() {
+    it("enter a string and recieve a string", function() {
+        expect(sentanceMill("hello world")).to.be.a("string");
+    });
 });
