3ec518a7ce2d59c26adddbefd4f5a56e98d48260
Charles Moss & Brian Borealis <student@epicodus.com>
2015-09-09 10:45:51 -0700

    actually passes 2nd test

--- js/main.js
+++ js/main.js
@@ -1,9 +1,15 @@
 var pigLatin = function(input) {
 
+
+    var firstLetter = input.slice(0, 1);
     var vowels = ["a", "e", "i","o","u"];
-    if (input.indexOf(vowels)){
+
+    if (vowels.indexOf(firstLetter) > -1) {
+
         input = input + "ay";
     };
+
+
 
     return input;
 };
